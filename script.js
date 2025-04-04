14a8f83b741a334ad60fae89c63978e033cf86df
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-04-04 12:37:58 +0200

    Fetch API añadido para que recorra correctamente la nueva variable global books

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 // Elements obtained from the DOM
-const contenedor2 = document.getElementById("contenedor2")
-
-
+const contenedor2 = document.getElementById("contenedor2");
 
 // Functions
 function mostrarResultados() {
-    // Global array 'books' must exist
-    for (let index = 0; index < books.length; index++) {
-        const book = books[index]
-        const card = document.createElement('div')
-        card.classList.add('card')
-        const imageURL = `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`
-        console.log(imageURL);
-        
+    let books = new Array();
+    fetch(`https://openlibrary.org/search.json?q=${searchBox.value}&limit=10`)
+        .then((resp) => resp.json())
+        .then((data) => {
+            books = data.docs;
+            for (let index = 0; index < books.length; index++) {
+                const book = books[index];
+                const card = document.createElement("div");
+                card.classList.add("card");
+                const imageURL = `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`;
+                console.log(imageURL);
 
-        const authors = book.author_name.join(", ")
-        card.innerHTML = `
-            <div class="card-image">
-                <img src="${imageURL}" alt="${book.title}">
-            </div>
-            <div class="card-content">
-                <h2 class="card-title">${book.title}</h2>
-                <p><i>${authors}</i> (${book.first_publish_year})</p>
-                <p><button id="${index}" onclick="addToCart(${index})">Añadir al carrito</button></p>
-            </div>
-        `
-        contenedor2.appendChild(card)
-    }
+                const authors = book.author_name.join(", ");
+                card.innerHTML = `
+                    <div class="card-image">
+                        <img src="${imageURL}" alt="${book.title}">
+                    </div>
+                    <div class="card-content">
+                        <h2 class="card-title">${book.title}</h2>
+                        <p><i>${authors}</i> (${book.first_publish_year})</p>
+                        <p><button id="${index}" onclick="addToCart(${index})">Añadir al carrito</button></p>
+                    </div>
+                `;
+                contenedor2.appendChild(card);
+            }
+        });
 }
 
 function getCartBooks() {
     if (!localStorage.cart) {
-        localStorage.cart = []
+        localStorage.cart = [];
     }
-    let cart = JSON.parse(localStorage.cart)
-    return cart
+    let cart = JSON.parse(localStorage.cart);
+    return cart;
 }
 
 function addToCart(index) {
     // The book to add to the cart is books[index]
     // First get the cart contents
-    let cart = getCartBooks()
-    cart.push(books[index])
-    localStorage.cart = JSON.stringify(cart)
+    let cart = getCartBooks();
+    cart.push(books[index]);
+    localStorage.cart = JSON.stringify(cart);
 }
 
 // mostrarResultados()
