57d2cd8f25543fc4e5fa0716a447eb4bcb0d3709
xescrihuelc <xescrihuelc@outlook.com>
2025-04-05 19:37:51 +0200

    function viewCart() modified & function findBookQuantity() added

--- script.js
+++ script.js
@@ -40,50 +40,50 @@ function mostrarResultados(books) {
                         <p><i>${authors}</i> (${book.first_publish_year})</p>
                         <span><button id="Add${index}" class="add-button" onclick="addToCart(${index})">Añadir al carrito</button><button id="Del${index}" class="del-button hidden" onclick="deleteFromCart(${index})">Eliminar del carrito</button></span>
                     </div>
-                `
-        contenedor2.appendChild(card)
+                `;
+        contenedor2.appendChild(card);
     }
-    contenedor2.classList.remove("hidden")
+    contenedor2.classList.remove("hidden");
 }
 
 function getCartBooks() {
     if (!localStorage.cart) {
         localStorage.cart = "[]";
     }
-    let cart = JSON.parse(localStorage.cart)
-    return cart
+    let cart = JSON.parse(localStorage.cart);
+    return cart;
 }
 
 function addToCart(index) {
     // The book to add to the cart is books[index]
-    const addButton = document.getElementById(`Add${index}`)
-    const delButton = document.getElementById(`Del${index}`)
+    const addButton = document.getElementById(`Add${index}`);
+    const delButton = document.getElementById(`Del${index}`);
     // First get the cart contents
-    let cart = getCartBooks()
-    cart.push(books[index])
-    localStorage.cart = JSON.stringify(cart)
-    updateCartCounter()
-    addButton.classList.add("hidden")
-    delButton.classList.remove("hidden")
+    let cart = getCartBooks();
+    cart.push(books[index]);
+    localStorage.cart = JSON.stringify(cart);
+    updateCartCounter();
+    addButton.classList.add("hidden");
+    delButton.classList.remove("hidden");
 }
 
 function deleteFromCart(index) {
     // The book to delete from the cart is books[index]
-    const addButton = document.getElementById(`Add${index}`)
-    const delButton = document.getElementById(`Del${index}`)
+    const addButton = document.getElementById(`Add${index}`);
+    const delButton = document.getElementById(`Del${index}`);
     // First get the cart contents
-    let cart = getCartBooks()
+    let cart = getCartBooks();
     // We have to find this book in the cart
     for (let cartIndex = 0; cartIndex < cart.length; cartIndex++) {
         if (books[index].key === cart[cartIndex].key) {
-            cart.splice(cartIndex, 1)
-            break
+            cart.splice(cartIndex, 1);
+            break;
         }
     }
-    localStorage.cart = JSON.stringify(cart)
-    updateCartCounter()
-    addButton.classList.remove("hidden")
-    delButton.classList.add("hidden")
+    localStorage.cart = JSON.stringify(cart);
+    updateCartCounter();
+    addButton.classList.remove("hidden");
+    delButton.classList.add("hidden");
 }
 
 function updateCartCounter() {
@@ -92,9 +92,37 @@ function updateCartCounter() {
 }
 
 function viewCart() {
+    // Get items from the cart
+    const cartItems = getCartBooks();
+    const bookTitle = Array();
+    const bookImage = Array();
+    const bookQuantity = Array();
+    console.log(cartItems);
+
+    cartItems.forEach((book) => {
+        bookTitle.push(book.title);
+        bookImage.push(
+            `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`
+        );
+        bookQuantity.push(findBookQuantity(book.title));
+    });
+    console.log(bookTitle, bookImage, bookQuantity);
+
+    // Hide containers 1 & 2 and unhide container 3
     contenedor1.classList.add("hidden");
     contenedor2.classList.add("hidden");
     contenedor3.classList.remove("hidden");
+}
+
+function findBookQuantity(title_book) {
+    let countBook = 0;
+    const books = getCartBooks();
+    books.forEach((book) => {
+        if (book.title == title_book) {
+            countBook++;
+        }
+    });
+    return countBook;
 }
 
 // mostrarResultados()
